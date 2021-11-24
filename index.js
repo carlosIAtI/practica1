1708b9803c0b21b12a0fb2fff3f0519672c5bad5
carlos_hdez.mtz <carlos_hdez.mtz@outlook.com>
2021-11-24 16:39:08 -0600

    s30 middlewares con express

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 // 1. Importar el modulo http
 import http from "http";
 import routes from "./routes.js"
-/** * Codigos de Emojies * Ref: https://www.w3schools.com/charsets/ref_emoji.asp 
-*/// 2. Crear el servidor// cb (callback) es una *funcion* que se ejecutara
+import Expres from 'express'
 
-const server = http.createServer(routes.requestHandler); 
-// 3. Pongo a trabajar el servidor
-// le paso un callback que escribira en la consola
-// cuando el servidor este escuchando//
-// 192.168.100.11:3000
-server.listen(3000, "0.0.0.0", () => {
-  console.log("👩‍🍳 Servidor escuchando en http://0.0.0.0:3000");
+//Crear una instancia de express
+const app = Expres();
+
+//Registrando el primer middleware
+app.use((req, res, next)=>{
+  console.log("Estoy en el middleware 1");
+  // Dar la instruccion de pasar al siguiente middleware
+  next()
+});
+app.use((req, res, next)=>{
+  console.log("Estoy en el middleware 2");
+  // Dar la instruccion de pasar al siguiente middleware
+  next()
+});
+
+app.use((_, res)=>{
+  console.log("Estoy en el middleware 3");
+  console.log("Emitiendo respuestas a cliente");
+  res.end("<h1>Hola Mundo!!</h1>");
+  // Dar la instruccion de pasar al siguiente middleware
+  
+});
+
+// poniendo a escuchar la aplicacion de express
+app.listen(3000, 'localhost',() => {
+  console.log("👩‍🍳 Servidor  escuchando en http://0.0.0.0:3000");
 });
